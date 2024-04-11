0a8f3a4f73967cc083f53ac402a6ababae66f12e
yangtory <wjdduscldrn@naver.com>
2024-04-11 23:55:10 +0900

    css

--- Team-Mem/src/main/java/com/callor/hello/HomeController.java
+++ Team-Mem/src/main/java/com/callor/hello/HomeController.java
@@ -20,16 +20,8 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	public String home(Locale locale, Model model) {	
+		return "layout";
 	}
 	
 }
---
+++ Team-Mem/src/main/webapp/WEB-INF/views/layout.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+<!DOCTYPE html>
+<html>
+<%@ include file="/WEB-INF/views/include/head.jspf"%>
+<body>
+	<%@ include file="/WEB-INF/views/include/header.jspf"%>
+	<div class="main">
+		<%@ include file="/WEB-INF/views/include/sidebar.jspf"%>
+		<section></section>
+	</div>
+</body>
+</html>
