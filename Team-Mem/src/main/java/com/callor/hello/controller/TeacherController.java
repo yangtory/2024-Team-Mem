c5fee0005669edbb5e8467e1dfc8ce18f65cfcaf
yangtory <wjdduscldrn@naver.com>
2024-04-12 20:00:11 +0900

    teacher

--- Team-Mem/src/main/java/com/callor/hello/controller/TeacherController.java
+++ Team-Mem/src/main/java/com/callor/hello/controller/TeacherController.java
@@ -1,16 +1,28 @@
 package com.callor.hello.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.callor.hello.dao.TeacherDao;
+import com.callor.hello.models.TeacherVO;
+
 @Controller
 @RequestMapping(value="/teacher")
 public class TeacherController {
 	
+	private final TeacherDao teacherDao;
+	public TeacherController(TeacherDao teacherDao) {
+		this.teacherDao = teacherDao;
+	}
+	
 	@RequestMapping(value={"/",""}, method=RequestMethod.GET)
 	public String home(Model model) {
+		List<TeacherVO> list = teacherDao.selectAll();
+		model.addAttribute("LIST", list);
 		model.addAttribute("BODY","TEACHER_HOME");
 		return "layout";
 	}
--- Team-Mem/src/main/webapp/WEB-INF/views/teacher/home.jsp
+++ Team-Mem/src/main/webapp/WEB-INF/views/teacher/home.jsp
@@ -1,3 +1,14 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<h1>ㅎㅇ </h1>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<div>
+	<h1>강사 리스트</h1>
+	<c:forEach items="${LIST}" var="T" varStatus="INDEX">
+		<ul>
+			<li>${INDEX.count}</li>
+			<li>${T.t_code}</li>
+			<li>${T.t_name}</li>
+		</ul>
+	</c:forEach>
+
+</div>
