125217cb6ac23fc34914af2b28bb0982ed2abcfb
n96js <n96js@naver.com>
2024-05-17 13:10:47 +0900

    수정

---
+++ Team-Mem/src/main/java/com/callor/hello/controller/TestController.java
@@ -0,0 +1,43 @@
+package com.callor.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.callor.hello.dao.UserCompDao;
+import com.callor.hello.dao.UserMinfoDao;
+import com.callor.hello.service.TeacherService;
+
+@Controller
+@RequestMapping(value="/test")
+public class TestController {
+
+	private final UserMinfoDao userMinfoDao;
+	private final TeacherService teacherService;
+	private final UserCompDao userCompDao;
+	
+	public TestController(UserMinfoDao userMinfoDao, TeacherService teacherService, UserCompDao userCompDao) {
+		super();
+		this.userMinfoDao = userMinfoDao;
+		this.teacherService = teacherService;
+		this.userCompDao = userCompDao;
+	}
+
+
+
+
+
+	@ResponseBody
+	@RequestMapping(value="/get", method=RequestMethod.GET)
+	public String test() {
+		
+	    Integer total = userMinfoDao.total("C0001");
+	    String count = userCompDao.count("C0001");
+	    if (total == null) {
+	        total = 0;
+	    }
+		return total.toString();
+	}
+	
+}
